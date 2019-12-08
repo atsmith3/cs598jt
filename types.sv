521a809f122c8b801b7b120a823e55843f80f4c9
atsmith3 <atsmith3@illinois.edu>
2019-12-08 17:47:54 -0600

    Going to merge

--- types.sv
+++ types.sv
@@ -16,3 +16,15 @@ typedef struct packed {
   logic last_vertex;
   logic last_edge;
 } pipeline_data_t;
+
+typedef struct packed {
+  int unsigned tick;
+  int unsigned ready, mem_flag, send; // _ready, _mem_flag, downstream_can_accept
+  int unsigned mem_result;       // mem_result
+  int unsigned vertex_id, vertex_dst_id, edge_id;
+  int unsigned vertex_data, vertex_dst_data, message_data;
+  shortreal edge_data, edge_temp_data;
+  int unsigned last_vertex, last_edge;
+} sim_event_t;
+
+string sim_event_format_str = "%d,%d,%d,%d,%d,%d,%lf,%lf,%d,%d,%d,%d,%d,%d\n";
