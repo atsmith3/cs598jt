f0f1dc5ff973338f92817a278f95b4a8d02ea63e
Jingyang Liu <jliu128@illinois.edu>
2019-12-19 18:58:10 -0600

    upload .tcl for .sdc generation

---
+++ generateSDC.tcl
@@ -0,0 +1,8 @@
+project_open -force "/home/jliu128/cs598/cs598jt_scripts/graphicionado_rtl.qpf" -revision graphicionado_rtl
+create_timing_netlist -model slow
+create_clock -name clk -period 10.000 [get_ports {clk}]
+derive_clock_uncertainty
+set_input_delay -clock { clk } 0 [all_inputs]
+set_output_delay -clock { clk } 0 [all_outputs]
+update_timing_netlist
+write_sdc -expand "top.sdc"
